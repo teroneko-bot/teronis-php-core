7ad4f4a4d6c45c49904941ba73dfc78d123ec564
C. Loncar <cloncar@isis-ic.com>
2018-10-08 08:41:34 +0100

    array_merge_recursive_distinct function added and changed by my self;

--- src/functions.php
+++ src/functions.php
@@ -15,7 +15,7 @@ function parseByteVariant(string $bytesVariant, $byteUnit = ""): float
     if (!$byteUnit) {
         $result = preg_match("/[a-zA-Z]+(?![0-9])$/", $bytesVariant, $matches);
 
-        $doesByteUnitExist = function (&$_byteUnit) use($matches) {
+        $doesByteUnitExist = function (&$_byteUnit) use ($matches) {
             $_byteUnit = strtolower($matches[0]);
             return array_key_exists($_byteUnit, BYTE_UNITS);
         };
@@ -71,4 +71,49 @@ function _getByteUnit(float $bytesVariant): string
     $pow = floor(log($bytesVariant) / log(1024));
     $byteUnit = array_search($pow, BYTE_UNITS);
     return $byteUnit;
+}
+
+/**
+ * array_merge_recursive does indeed merge arrays, but it converts values with duplicate
+ * keys to arrays rather than overwriting the value in the first array with the duplicate
+ * value in the second array, as array_merge does. I.e., with array_merge_recursive,
+ * this happens (documented behavior):
+ *
+ * array_merge_recursive(array('key' => 'org value'), array('key' => 'new value'));
+ *     => array('key' => array('org value', 'new value'));
+ *
+ * array_merge_recursive_distinct does not change the datatypes of the values in the arrays.
+ * Matching keys' values in the second array overwrite those in the first array, as is the
+ * case with array_merge, i.e.:
+ *
+ * array_merge_recursive_distinct(array('key' => 'org value'), array('key' => 'new value'));
+ *     => array('key' => array('new value'));
+ *
+ * Parameters are passed by reference, though only for performance reasons. They're not
+ * altered by this function.
+ *
+ * @param array $array1
+ * @param array $array2
+ * @return array
+ * @author Daniel <daniel (at) danielsmedegaardbuus (dot) dk>
+ * @author Gabriel Sobrinho <gabriel (dot) sobrinho (at) gmail (dot) com>
+ * @author C. Loncar <giteroneko (at) gmail (dot) com>
+ */
+function array_merge_recursive_distinct(array &$array1, array &$array2, bool $applyChangesOnArray1 = false): array
+{
+    if ($applyChangesOnArray1) {
+        $merged = &$array1;
+    } else {
+        $merged = $array1;
+    }
+
+    foreach ($array2 as $key => &$value) {
+        if (is_array($value) && isset($merged[$key]) && is_array($merged[$key])) {
+            $merged[$key] = array_merge_recursive_distinct($merged[$key], $value);
+        } else {
+            $merged[$key] = $value;
+        }
+    }
+
+    return $merged;
 }
